1f6b27be74697d008180578e9bc9451ea72c6943
salah <jamesetma@gmail.com>
2022-10-01 03:42:46 -0700

    edited

--- lib/views/pages/landing_page.dart
+++ lib/views/pages/landing_page.dart
@@ -1,19 +1,42 @@
+import 'package:alsham_socialmedia/constants/app_colors.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:iconly/iconly.dart';
 
 class LandingPage extends StatelessWidget {
-  const LandingPage({Key? key}) : super(key: key);
-
+  LandingPage({Key? key}) : super(key: key);
+  // RxInt seveen = 0.obs;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: BottomNavigationBar(items: [
-        BottomNavigationBarItem(
-          label: 'Home',
-          icon: Icon(Icons.home),
-        ),
-        BottomNavigationBarItem(label: 'Add', icon: Icon(Icons.add)),
-        BottomNavigationBarItem(label: 'Profile', icon: Icon(Icons.person))
-      ]),
+      bottomNavigationBar: Obx(
+        () => BottomNavigationBar(
+            // onTap: (val) {
+            //   seveen.value = val;
+            // },
+            iconSize: 30,
+            // currentIndex: seveen.value,
+            showUnselectedLabels: false,
+            showSelectedLabels: false,
+            selectedItemColor: AppColors.appPrimary,
+            items: const [
+              BottomNavigationBarItem(
+                label: 'Home',
+                icon: Icon(IconlyLight.home),
+                activeIcon: Icon(IconlyBold.home),
+              ),
+              BottomNavigationBarItem(
+                label: 'Add',
+                icon: Icon(IconlyLight.plus),
+                activeIcon: Icon(IconlyBold.plus),
+              ),
+              BottomNavigationBarItem(
+                label: 'Profile',
+                icon: Icon(IconlyLight.profile),
+                activeIcon: Icon(IconlyBold.profile),
+              ),
+            ]),
+      ),
     );
   }
 }
